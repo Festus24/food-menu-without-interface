03f4c29446942f7097db8551715512ae76ec227c
Festus <ojofestus2030@gmail.com>
2026-04-09 09:09:30 +0100

    Add meal editing and deletion functionality with modal support

--- script.js
+++ script.js
@@ -28,11 +28,38 @@ function addMeal(){
         document.getElementById('mealInput').value = ''
     }
 }
+function deleteMeal(i){
+    const confirmation = confirm('Are you sure you want to delete?')
+    if(confirmation){
+        allMeal.splice(i, 1)
+        showMeal()
+    }
+    else{
+        showMeal()
+    }
+}
+
+function editMeal(i){
+    editIndex = i
+    editMealinput.value = allMeal[i]
+}
+
+function saveEdit(){
+    const mealEditt = document.getElementById('editMealinput').value
+    if(mealEditt.trim() == ''){
+        alert('input cannot be empty')
+    }
+    else{
+        allMeal.splice(editIndex, 1, mealEditt)
+        showMeal()
+        document.getElementById('editMealinput').value = ''
+    }
+}
 
 function showMeal(){
     show.innerHTML = ''
     for (let index = 0; index < allMeal.length; index++) {
         const element = allMeal[index];
-        show.innerHTML += `<p>${index + 1}. ${element} successfully cooked</p>`
+        show.innerHTML += `<p>${index + 1}. ${element} successfully cooked <button onclick="deleteMeal()">Delete</button> <button onclick="editMeal(${index})" data-bs-toggle="modal" data-bs-target="#exampleModal">Edit</button></p>`
     }
 }
